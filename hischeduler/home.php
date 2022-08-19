18fe98f3612d1f4ee14fe67b1bbb02b6254d92c5
SEACLUE <75559644+SEACLUE@users.noreply.github.com>
2022-08-19 20:48:54 +0900

    バックアップ(地域機能まで)
    地域追加, 管理者ログインまで完成しました。現時点ではバグやセキュリティー上の問題等はありません。

---
+++ hischeduler/home.php
@@ -0,0 +1,42 @@
+<?php
+// global error list
+$error = array();
+// Import default settings
+require_once('./config.php');
+session_start();
+// If not logged in, redirect to index.php
+if ($_SESSION['LOGGED_IN'] != 1) {
+  header('Location: ./index.php');
+}
+// Logout process
+if ($_POST['logout'] == 'ログアウト') {
+  $_SESSION = array();
+  header('Location: ./index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="manifest" href="./manifest.json">
+  <link rel="shortcut icon" href="./images/logo256.png">
+  <script src="./jquery.js"></script>
+  <title>HiScheduler || 研修スケジューラー</title>
+</head>
+
+<script>
+  $(document).ready(function() {
+
+  });
+</script>
+
+<body>
+  <form method="post">
+    <input type="submit" value="ログアウト" name="logout">
+  </form>
+</body>
+
+</html>
